25a43bf9a2f5831d88b8b8386096cb86010ede4e
Marco Fan <fanch_-dev@yahoo.com.hk>
2012-02-07 14:25:59 +0800

    user_model: update function

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -31,12 +31,12 @@ class User_model extends CI_Model {
 	 *
 	 * param	*
 	 * user		an array containing the user information which to be inserted into the DB, 
-	 *							the array keys must match the DB fields.
-	 * 						If this parameter is not provided, this method reads from the html post.
+	 *				the array keys must match the DB fields.
+	 * 			If this parameter is not provided, this method reads from the html post.
 	 *
 	 * return *
 	 * mixed		return an array containing the user array;
-	 *							return false if error occurs
+	 *				return false if error occurs
 	 */
 	public function insert_user($user = FALSE) {
 		if ( $user === FALSE ) {
@@ -44,7 +44,6 @@ class User_model extends CI_Model {
 			$user['lastname'] = $this->input->post('lastname');
 			$user['email'] = $this->input->post('email');
 			$user['pwd'] = md5($this->input->post('pwd'));
-			$conpwd = $this->input->post('conpwd');
 			$user['phone'] = $this->input->post('phone');
 			$user['gender'] = $this->input->post('gender');
 			
@@ -57,6 +56,47 @@ class User_model extends CI_Model {
 		}
 		else if ( is_array($user) ) {
 			$query = $this->db->insert('users', $user);
+			return $user;
+		}
+		
+		return FALSE;
+	}
+	
+	/********************************
+	 * Update users in the database
+	 *
+	 * param	*
+	 * id			the user id used for the update operation
+	 * user		an array containing the user information which to be inserted into the DB, 
+	 *				the array keys must match the DB fields.
+	 * 			If this parameter is not provided, this method reads from the html post.
+	 *
+	 * return *
+	 * mixed		return an array containing the user array;
+	 *				return false if error occurs
+	 */
+	public function update_user($id = '', $user = FALSE) {
+		if ( $id === '' ) {
+			return FALSE;
+		}
+		
+		$this->db->where('id', $id);
+		
+		if ( $user === FALSE ) {
+			$user['firstname'] = $this->input->post('firstname');
+			$user['lastname'] = $this->input->post('lastname');
+			$user['email'] = $this->input->post('email');
+			$user['pwd'] = md5($this->input->post('pwd'));
+			$user['phone'] = $this->input->post('phone');
+			$user['gender'] = $this->input->post('gender');
+			
+			$user['modified_time'] = date( 'Y-m-d H:i:s', time() );
+			$user['role_id'] = 1;
+			$this->db->update('users', $user);
+			return $user;
+		}
+		else if ( is_array($user) ) {
+			$this->db->update('users', $user);
 			return $user;
 		}
 		
