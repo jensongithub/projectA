4ead8f3327baf2d42c2ee097b3aedc1da0704979
Jenson <jenson@jupiter-jazz.com>
2012-02-08 15:37:15 +0800

    fixed location.php

--- application/views/pages/location.php
+++ application/views/pages/location.php
@@ -5,11 +5,21 @@
 	<tbody>
 		<tr>
 			<td>尖沙咀店</td><td>九龍尖沙咀加連威老道27號嘉榮大廈地下A鋪</td><td>23327006</td>
+		</tr>
+		<tr>
 			<td>太古店</td><td>康怡廣場(南座)吉之島1樓女裝部104A</td><td>29410469</td>
+		</tr>
+		<tr>
 			<td>黃埔店</td><td>黃埔吉之島地庫B6-10</td><td>34881638</td>
+		</tr>
+		<tr>
 			<td>屯門店</td><td>屯門市廣場吉之島1期1樓L330D</td><td>34883198</td>
+		<tr>
 			<td>九龍灣店</td><td>MEGA BOX 吉之島 L117</td><td>34883178</td>
+		</tr>
+		<tr>
 			<td>元朗店</td><td>千色CITISTORE 3樓女裝部C18</td><td>24703880</td>
 		</tr>
 	</tbody>
+</table>
 </div>
