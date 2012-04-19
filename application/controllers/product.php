6356ced02c4b9b61a90202c395640dac46707c6b
Jenson <jenson@jupiter-jazz.com>
2012-04-19 19:17:25 +0800

    add checkout for paypal will add cart soon

---
+++ application/controllers/product.php
@@ -0,0 +1,28 @@
+<?php
+class Product extends CI_Controller {
+	var $data=array();
+	public function __construct()	{
+		parent::__construct();
+		$this->load->model('user_model');
+		$this->load->library('email');
+		$this->data = array_merge($this->data, $this->session->all_userdata());
+	}
+
+	public function index()	{
+		$data['title'] = 'Product Catalog';
+		
+		$this->load->helper( array('form') );
+		//$this->lang->load('register');
+		$this->load->library('form_validation', 'session',"paypalExCheckout");
+		$this->
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/product', $this->data);
+		$this->load->view('templates/footer');
+	}
+	
+	public function checkout(){
+		$this->load->library("paypalExCheckout");
+		$this->paypalExCheckout->checkout();
+	}
+	
+}
---
+++ application/views/paypal/form.php
@@ -0,0 +1,2 @@
+<h1>PayPal Library for Code Igniter</h1>
+<?php echo $paypal_form; ?>
