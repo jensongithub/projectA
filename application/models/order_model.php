914da7f493b1c1edfd114745714b28543ae6b58a
Jenson <jenson@jupiter-jazz.com>
2012-05-07 22:45:16 +0800

    checkout save paypal address

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -10,15 +10,26 @@ class Order_model extends CI_Model {
 
 	public function update_paypal_order() {
 		// update the temporary order to complete order
-		$timestamp = strtotime($_POST['payment_date']);
+		$timestamp = strtotime($this->CI->input->post('payment_date'));
 		$payment_date = date("Y-m-d H:i:s",$timestamp);
 		
-		$query = "update orders set txn_id = ?, status = ?, payment_date=?, modified_date = ? where rand_key = ? and user_id = ? and id = ?";
 		
-		$result = $this->db->query($query, array($_POST['txn_id'], $_POST['payment_status'], $payment_date, $payment_date, $_POST['invoice'], $_POST['custom'], $_POST['parent_txn_id']));
+		// custom is the user id in the production database
+		//$this->CI->input->post('custom')
 		
-		$query = "update orders_items set txn_id = ?, total_amount = ? where rand_key = ? and txn_id is null and order_id = ?";
-		$result = $this->db->query($query, array($_POST['txn_id'], $_POST['mc_gross'], $_POST['invoice'], $_POST['parent_txn_id']));
+		// parent_txn_id is the order id 
+		//$this->CI->input->post('parent_txn_id')
+		
+		//$this->CI->input->post('invoice') is the rand_key
+		
+		$query = "insert into order_address(order_id, user_id, country, country_code, country_zip, country_state, country_city, country_street, created_date, modified_date) values(?, ?, ?, ?,?,?,?,?,NOW(), NOW())";
+		$result = $this->db->query($query, array($this->CI->input->post('parent_txn_id'), $this->CI->input->post('custom'), $this->CI->input->post('address_country'), $this->CI->input->post('address_country_code'), $this->CI->input->post('address_zip'), $this->CI->input->post('address_state'), $this->CI->input->post('address_city'), $this->CI->input->post('address_street')));
+
+		$query = "update orders set txn_id = ?, total_amount=?, status = ?, payment_date=?, modified_date = ? where rand_key = ? and user_id = ? and id = ?";
+		$result = $this->db->query($query, array($this->CI->input->post('txn_id'), $this->CI->input->post('mc_gross'), $this->CI->input->post('payment_status'), $payment_date, $payment_date, $this->CI->input->post('invoice'), $this->CI->input->post('custom'), $this->CI->input->post('parent_txn_id')));
+		
+		$query = "update orders_items set txn_id = ?, modified_date = NOW() where rand_key = ? and order_id = ?";
+		$result = $this->db->query($query, array($this->CI->input->post('txn_id'), $this->CI->input->post('invoice'), $this->CI->input->post('parent_txn_id')));
 	}
 	
 	function insert_checkout_item($data){
@@ -48,7 +59,7 @@ class Order_model extends CI_Model {
 		/*$timestamp = strtotime($_POST['payment_date']);
 		$payment_date = date("Y-m-d H:i:s",$timestamp);
 		*/
-				
+		
 		$query = "insert into orders(user_id, rand_key, status, created_date, modified_date) values(?, ?, ?, NOW(), NOW())";
 		$result = $this->db->query($query, array($user['id'], $rand_key, 'pending'));
 		
@@ -68,5 +79,25 @@ class Order_model extends CI_Model {
 //		$affected_rows += count($this->db->affected_rows());
 		
 		return array("rand_key"=>$rand_key, "order_id"=>$order_id);
-	}	
+	}
+	
+	function get_order_by_status($condition, $rownum, $howmany){
+		$condition_str = '';		
+		foreach ($condition as $key=>$val){			
+			//$val = $each_cond[$key];
+			$condition_str .= " and $key = '$val' ";
+		}
+		
+		$query = "sselect users.firstname, users.lastname, users.email, orders.* from orders, users, order_address where  orders.user_id = users.id and orders.address_id = order_address.id and orders.status='Completed' $condition_str limit ?, ?";
+		$query = $this->db->query($query, array( $rownum, $howmany) );
+		
+		return $query->result_array();
+	}
+	
+	function get_order_items_by_order_id($order_id){
+		
+		//SELECT cat.* FROM products pro, categories cat, product_category pc WHERE pro.id = pc.pro_id AND cat.id = pc.cat_id AND pro.id = ?
+		
+		//return json_encode($query->result_array(), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_FORCE_OBJECT);
+	}
 }
